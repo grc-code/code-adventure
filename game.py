f144cadb2b751a6ec597c32dcddfb2d7c3cb8feb
Evan Wheeler <effyn@tutanota.com>
2019-11-18 13:22:51 -0800

    Second feature, multiple enemies, stat reporting
    This Code Day, we explored how to use dictionaries and lists to make our code more maintainable and flexible. Now the player has to fight multiple enemies, and the damage done and health left is reported to the player.

--- game.py
+++ game.py
@@ -9,57 +9,77 @@ alive = True
 # whether we are processing an action
 doing_action = False
 
-player_health = 10
-player_attack = 15
+player = {
+    "health": random.randint(10, 40),
+    "weapons": [
+        {
+            "name": "Dagger",
+            "attack": random.randint(10, 20),
+        },
+    ],
+}
+
+enemies = []
+
+max_enemies = 2
 
 # While the player is alive
 while alive:
 
     # Prompt the player for an action
-    action = input("Enter a number: 1 - Attack, 2 - Item, 3 - Exit")
+    action = input("Enter a number: 1 - Attack, 2 - Item, 3 - Exit > ")
 
     # If the player wants to attack
     if action == "1":
 
-        # enemy health = random value from 10 to 100
-        enemy_health = random.randint(10, 100)
+        for _ in range(random.randint(1, max_enemies + 1)):
+            enemy = {
+                "name": random.choice(["Goomba", "Koopa", "Paratroopa"]),
+                "health": random.randint(10, 100),
+                "attack": random.randint(1, 10),
+            }
+            enemies.append(enemy)
 
-        # Print out something special if the enemy has max health
-        if enemy_health == 100:
-            print("Deathwing is coming...")
+            # Print out something special if the enemy has max health
+            if enemy["health"] == 100:
+                print("Deathwing is coming...")   
 
         # Start the battle
         doing_action = True
         while doing_action:
+            weapon = player["weapons"][0]
+            enemy = enemies[0]
 
-            # Calculate enemy attack (from 0 to 5)
-            enemy_attack = random.randint(0, 5)
+            input("Press enter to attack the first enemy!\n")
 
-            # Lose health
-            player_health = player_health - enemy_attack
+            enemy["health"] -= weapon["attack"]
 
-            print("A Goomba attacks! Lost X health, Y remaining.")
+            print(f"You attacked the {enemy['name']}! Lost {weapon['attack']} health, {enemy['health']} remaining.")
 
-            # If the player has died
-            if player_health <= 0:
-                print("You died...")
-                # leave the action
-                doing_action = False
-                # set alive to False so we exit the main loop
-                alive = False
+            # if the enemy has died
+            if enemy["health"] <= 0:
+                print(f"You killed the {enemy['name']}!")
+                
+                del enemies[0]
 
-            else:
-                input("Press enter to attack!")
-                # Make the enemy lose health
-                enemy_health = enemy_health - player_attack
-
-                print("You attacked the Goomba! Lost X health, Y remaining.")
-
-                # if the enemy has died
-                if enemy_health <= 0:
+                if enemies == []:
                     print("You won the battle!")
                     # leave just the action, not the main loop
                     doing_action = False
+                    continue
+
+            for enemy in enemies:
+                player["health"] -= enemy["attack"]
+
+                print(f"A {enemy['name']} attacks! Lost {enemy['attack']} health, {player['health']} remaining.")
+
+                # If the player has died
+                if player["health"] <= 0:
+                    print("You died...")
+                    # leave the action
+                    doing_action = False
+                    # set alive to False so we exit the main loop
+                    alive = False
 
     elif action == "2":
         # TODO impl item logic
